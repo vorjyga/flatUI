946c18bf0dabc621c19f0925cf9c130e7f563c88
vorjyga <novaikin@gmail.com>
2017-03-20 18:27:51 +0700

    Сделал кнопочки

--- frontend/blocks/button/button.js
+++ frontend/blocks/button/button.js
@@ -1,7 +1,32 @@
-$(document).ready(function() {
-  $(".buttons__btn").mouseenter(function () {
-    $(".buttons__btn").animate({
-      height: '+=1em'
-    })
-  })
+$(document).ready(function () {
+  $('.buttons__btn').on('click', function () {
+    if ( $(this).hasClass('buttons__btn_a'))
+    {
+      if (!$(this).hasClass('buttons__btn_shadow-green') && $(this).hasClass('buttons__btn_green')) {
+        $(this).addClass('buttons__btn_shadow-green')
+      }
+      else if ($(this).hasClass('buttons__btn_green') && $(this).hasClass('buttons__btn_shadow-green')) {
+        $(this).removeClass('buttons__btn_green')
+        $(this).addClass('buttons__btn_white-green')
+      }
+      else {
+        $(this).removeClass('buttons__btn_white-green')
+        $(this).addClass('buttons__btn_green')
+        $(this).removeClass('buttons__btn_shadow-green')
+      }
+    }
+    else  {
+      if (!$(this).hasClass('buttons__btn_shadow-red') && $(this).hasClass('buttons__btn_red')) {
+        $(this).addClass('buttons__btn_shadow-red')
+      }
+      else if ($(this).hasClass('buttons__btn_red') && $(this).hasClass('buttons__btn_shadow-red')) {
+        $(this).removeClass('buttons__btn_red')
+        $(this).addClass('buttons__btn_white-red')
+      }
+      else {
+        $(this).addClass('buttons__btn_red')
+        $(this).removeClass('buttons__btn_shadow-red')
+        $(this).removeClass('buttons__btn_white-red')
+      }}
+  });
 });
--- public/main.js
+++ public/main.js
@@ -101,10 +101,30 @@ webpackContext.id = 0;
 /***/ (function(module, exports, __webpack_require__) {
 
 /* WEBPACK VAR INJECTION */(function($) {$(document).ready(function () {
-  $(".buttons__btn").mouseenter(function () {
-    $(".buttons__btn").animate({
-      height: '+=1em'
-    });
+  $('.buttons__btn').on('click', function () {
+    if ($(this).hasClass('buttons__btn_a')) {
+      if (!$(this).hasClass('buttons__btn_shadow-green') && $(this).hasClass('buttons__btn_green')) {
+        $(this).addClass('buttons__btn_shadow-green');
+      } else if ($(this).hasClass('buttons__btn_green') && $(this).hasClass('buttons__btn_shadow-green')) {
+        $(this).removeClass('buttons__btn_green');
+        $(this).addClass('buttons__btn_white-green');
+      } else {
+        $(this).removeClass('buttons__btn_white-green');
+        $(this).addClass('buttons__btn_green');
+        $(this).removeClass('buttons__btn_shadow-green');
+      }
+    } else {
+      if (!$(this).hasClass('buttons__btn_shadow-red') && $(this).hasClass('buttons__btn_red')) {
+        $(this).addClass('buttons__btn_shadow-red');
+      } else if ($(this).hasClass('buttons__btn_red') && $(this).hasClass('buttons__btn_shadow-red')) {
+        $(this).removeClass('buttons__btn_red');
+        $(this).addClass('buttons__btn_white-red');
+      } else {
+        $(this).addClass('buttons__btn_red');
+        $(this).removeClass('buttons__btn_shadow-red');
+        $(this).removeClass('buttons__btn_white-red');
+      }
+    }
   });
 });
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(3)))
