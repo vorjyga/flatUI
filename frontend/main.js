912a2b135e6abebf20095ec792fdd64136e95046
vorjyga <novaikin@gmail.com>
2017-03-30 11:27:15 +0700

    Create search-bar, toggles and checkboxes

--- frontend/main.js
+++ frontend/main.js
@@ -3,6 +3,7 @@
 import './main.styl';
 
 import '../node_modules/jquery-ui-bundle/jquery-ui.min';
+import '../node_modules/jquery-toggles/toggles.min'
 
 function importAll (r) {
   r.keys().forEach(r);
